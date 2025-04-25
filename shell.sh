dbd5abe1beb90cf6fc544f47bfcd5ed28164cb26
mellxssa <96584532+mellxssa@users.noreply.github.com>
2025-04-25 18:58:02 +0200

    fixing some issues

--- shell.sh
+++ shell.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
-
-# Define the list of services and their images
-declare -a services=(
-    "mongo:4.4"
-    "free5gc/nrf:v4.0.0"
-    "free5gc/amf:V4.0.0"
-    "free5gc/ausf:v4.0.0"
-    "free5gc/nssf:v4.0.0"
-    "free5gc/pcf:v4.0.0"
-    "free5gc/smf:v4.0.0"
-    "free5gc/udm:v4.0.0"
-    "free5gc/udr:v4.0.0"
-    "free5gc/chf:v4.0.0"
-    "free5gc/nef:latest"
-    "free5gc/webui:v4.0.0"
-    "free5gc/upf:latest"
-    "free5gc/ueransim:latest"
-    "ubuntu:20.04"
-)
-
-# Pull each image
-for image in "${services[@]}"; do
-    echo "Pulling image: $image"
-    docker pull $image
-    if [ $? -ne 0 ]; then
-        echo "Failed to pull image: $image"
-        exit 1
-    else
-        echo "Successfully pulled image: $image"
-    fi
+# Create proper certificates in ./cert directory
+mkdir -p cert
+cd cert
+openssl genrsa -out free5gc.key 2048
+openssl req -new -x509 -key free5gc.key -out free5gc.pem -subj "/CN=free5gc.org/O=Free5GC/C=TW" -days 3650
+# Create links for each NF
+for nf in amf smf upf nrf ausf nssf pcf udm udr n3iwf chf nef; do
+  cp  free5gc.key $nf.key
+  cp  free5gc.pem $nf.pem
 done
-
-echo "All images have been pulled successfully."
